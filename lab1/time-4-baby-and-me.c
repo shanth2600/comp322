43359d85d66b35cdbc308c1aac8a646a11c1f4e1
Shant Hairapetian <sh546459@comp322.sandbox.csun.edu>
2014-09-10 21:42:30 -0700

    Added source file

---
+++ lab1/time-4-baby-and-me.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/times.h>
+#include <string.h>
+#include <time.h>
+
+
+void startFork(){
+ struct tms cTms;
+ int cpid;
+ int stt;
+ times(&cTms);
+ cpid=fork();
+ waitpid(cpid,&stt,WCONTINUED);
+ printf("PID:%d",getpid());
+ printf(", PPID:%d",getppid());
+ if(cpid!=0){
+  printf(", CPID:%d",cpid);
+  printf(", RETVAL:%d\n",WEXITSTATUS(stt));
+  times(&cTms);
+  printf("USER:%d, SYS:%d, ",cTms.tms_utime,cTms.tms_utime);
+  printf("CUSER:%d, CSYS:%d",cTms.tms_cutime,cTms.tms_cstime);
+ }else{
+  int i,j;
+  for(i=0;i<100000000;i++)j=i;
+ }
+ printf("\n");
+ exit(0);
+
+
+}
+
+
+int main(){
+ printf("Start:%d\n",time(NULL));
+ startFork();
+ printf("Stop:%d\n",time(NULL));
+ return 0;
+}
+
