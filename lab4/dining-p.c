c8645882656651c2ceea94278bf223963120b524
Shant Hairapetian <sh546459@comp322.sandbox.csun.edu>
2014-11-01 19:54:33 -0700

    done with part 2

--- lab4/dining-p.c
+++ lab4/dining-p.c
@@ -12,8 +12,7 @@
 #include <errno.h>
 
 sem_t *chop[2];
-sem_t * returnVal;
-
+sem_t *maxPhil;
 
 int seats;
 int position;
@@ -21,35 +20,24 @@ int cycles=0;
 char semStr1[4];
 char semStr2[4];
 
-void wt(sem_t *sem){
- while(sem>0){}
- sem=0;
-}
-
-void sig(sem_t *sem){
- sem++;
-}
-
 void eat(){
  printf("Philosopher %d is eating\n",position);
  sleep(rand()%3);
 }
-
 void think(){
  printf("Philosopher %d is thinking\n",position);
  sleep(rand()%3);
 }
-
 void handler(){
  printf("Philosopher #%d completed %d cycles\n",position,cycles);
  sem_close(chop[0]);
  sem_unlink(semStr1);
  sem_close(chop[1]);
  sem_unlink(semStr2);
+ sem_close(maxPhil);
+ sem_unlink("maxPhil");
  exit(0);
 }
-
-
 int main(int argc, char *argv[]){
  int i;
  char cnum1,cnum2;
@@ -69,19 +57,20 @@ int main(int argc, char *argv[]){
   strcpy(semStr1,tmp1);
   char tmp2[]={'/','c',cnum2,'\0'};
   strcpy(semStr2,tmp2);
-  printf("%s %s\n",semStr1,semStr2);
   chop[0]=sem_open(semStr1, O_CREAT, 0666, 1);
   chop[1]=sem_open(semStr2, O_CREAT, 0666, 1);
-  printf("PID:%d\n",getpid());
+  maxPhil=sem_open("maxPhil", O_CREAT, 0666, 2);
   signal(15,handler);
   do{
-if(cycles==6)handler();
-   wait(chop[0]);
-   wait(chop[1]);
+   sem_wait(maxPhil);
+   sem_wait(chop[0]);
+   sem_wait(chop[1]);
    eat();
-   sig(chop[0]);
-   sig(chop[1]);
+   sem_post(chop[0]);
+   sem_post(chop[1]);
+   sem_post(maxPhil);
    think();
+   
    cycles++;
   }while(1);
  }else{
@@ -89,4 +78,3 @@ if(cycles==6)handler();
  }
  return 0;
 }
-
