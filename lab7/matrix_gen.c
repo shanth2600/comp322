951904b5e6f8015626e0192e6600699830957e65
Shant Hairapetian <sh546459@comp322.sandbox.csun.edu>
2014-11-26 17:48:20 -0800

    modified matrix gen to output binary numbers

--- lab7/matrix_gen.c
+++ lab7/matrix_gen.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int *buff=NULL;
+
 void theBluePill(int sz){
  int n=-100;
  int m=100;
@@ -11,10 +14,24 @@ void theBluePill(int sz){
  }
 }
 
+void fillBuff(int sz){
+ int i;
+ int n=-100;
+ int m=100;
+ srand(time(NULL));
+ for(i=0;i<sz;i++)buff[i]=rand()/(RAND_MAX/(n-m+1)+1);
+}
+
+FILE *f;
 int main(int argc,char *argv[]){
  int sz;
  if(argc>1){
-   theBluePill(atoi(argv[1]));
+   sz=atoi(argv[1]);
+   buff=malloc(sz*sizeof(int));
+   fillBuff(sz);
+   f=fdopen(1,"w");
+   fwrite(buff,sizeof(int),sz,f);
+   fclose(f);
   }else{
   printf("Not enough paramters, dumb dumb\n");
  }
