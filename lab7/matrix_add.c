6dfeb1d8b7a7ccc726dc241a2cf1c17bdd109852
Shant Hairapetian <sh546459@comp322.sandbox.csun.edu>
2014-11-26 18:21:09 -0800

    Somethings wrong

--- lab7/matrix_add.c
+++ lab7/matrix_add.c
@@ -3,6 +3,7 @@
 int **arr;
 
 void outarr(int sz){
+ printf("herererere\n");
  int i,j;
  for(i=0;i<sz;i++){
   for(j=0;j<sz;j++)printf("%d",arr[i][j]);
@@ -14,31 +15,40 @@ void construct(int sz){
  int i,j;
  arr=(int **)malloc(sz*sizeof(int *));
  for(i=0;i<sz;i++)arr[i]=(int *)malloc(sz*sizeof(int));
-// for(i=0;i<sz;i++){
-//  for(j=0;j<sz;j++)arr[i][j]=0;
-// }
-// outarr(sz);
+/*
+ for(i=0;i<sz;i++){
+  for(j=0;j<sz;j++)arr[i][j]=0;
+ }
+ outarr(sz);
+*/
+}
+
+int *buff;
+
+
+void fillArr(int sz){
+ int i,j;
+ for(i=0;i<sz;i++){
+  for(j=0;j<sz;j++)arr[i][j]=buff[(sz*i)+j];
+ }
 }
 
 
-
 int main(int argc,char *argv[]){
- int c,i=0;
- char *x;
+ int sz,c;
+ FILE *f;
  if(argc>1){
-  construct(atoi(argv[1]));
-  char buff[4];
-  while((c=fgetc(stdin))!= EOF){
-   buff[i]=(char)(((int)'0')+c);
-   printf("%c",buff[i]);
-   i++;
-   if(i==3){
-    sprintf(x,"%s",atoi(buff));
-    i=0;
-   }
-printf("%s\n",x);
-break;
-  }
+  sz=atoi(argv[1]);
+  construct(sz);
+  buff=malloc((sz*sz)*sizeof(int));
+  f=fdopen(0,"r+");
+
+  fwrite(buff,sizeof(int),sz*sz,f);
+  fclose(f);
+//  fillArr(sz);
+//printf("herere123\n");
+
+  //outarr(sz);
  }else{
   printf("Not enough parameters dumb dumb!\n");
  }
